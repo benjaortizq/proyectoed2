98a9fe5fca0b1e3a02ad6433f48c3f5586e20aed
Conoutlet <benjaoq@gmail.com>
2026-06-07 15:33:20 -0600

    add Galaxia and Ruta structures with constructors

---
+++ galaxia.cpp
@@ -0,0 +1,43 @@
+
+// estos son los nodos de la API o algo asi xd 
+
+#include <iostream> 
+using namespace std  ; 
+struct Galaxia  { 
+    string id  ; 
+    string codigo; 
+    string nombre ; 
+    string tipo ; 
+    float x; // mucho despiche harlo short , el parseado es un despiche 
+    float y; 
+    float z ; 
+    string descripcion  ;  // esta vaina es biel larga
+
+    public : 
+
+    Galaxia ()  {  // iniciador vacio , tiene que tener inicializador vacio a params igual 
+        this->id = "" ; 
+        this->codigo= "" ; 
+        this->nombre = "" ; 
+        this->tipo = "" ; 
+        this->descripcion = "" ;
+        this->x = 0 ; 
+        this->y = 0 ; 
+        this->z = 0 ; 
+    }
+
+    Galaxia (string id , string codigo , string nombre , string tipo , string descripcion , int x , int y , int z) { 
+        this->id = id ; 
+        this->codigo= codigo; 
+        this->nombre = nombre ; 
+        this->tipo = tipo ; 
+        this->descripcion = descripcion ;
+        this->x = x ; 
+        this->y = y ; 
+        this->z = z ; 
+    } ; 
+
+
+
+
+} ; 
---
+++ ruta.cpp
@@ -0,0 +1,4 @@
+// esto es litealmente las Aristas de lo que la API me da xd  
+#include <iostream> 
+using namespace std  ; 
+
