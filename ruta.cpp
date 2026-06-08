3d19c071202b092cb529e4779ad4c763a9fc2fed
Conoutlet <benjaoq@gmail.com>
2026-06-08 16:51:11 -0600

    add Ruta structure with default and parameterized constructors

--- ruta.cpp
+++ ruta.cpp
@@ -2,3 +2,33 @@
 #include <iostream> 
 using namespace std  ; 
 
+struct Ruta  { 
+    string id  ; 
+    string origen_id  ; 
+    string destino_id ; 
+    string tipo ; 
+    float costo ; 
+    float tiempo_dias; 
+    bool activa ;
+    public :
+    Ruta() {
+        this->id = "";
+        this->origen_id = "";
+        this->destino_id = "";
+        this->tipo = "";
+        this->costo = 0.0;
+        this->tiempo_dias = 0.0;
+        this->activa = false;
+    }
+
+    Ruta(string id, string origen_id, string destino_id, string tipo, float costo, float tiempo_dias, bool activa) {
+        this->id = id;
+        this->origen_id = origen_id;
+        this->destino_id = destino_id;
+        this->tipo = tipo;
+        this->costo = costo;
+        this->tiempo_dias = tiempo_dias;
+        this->activa = activa;
+    }
+
+};
